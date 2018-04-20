88a2822c498962b76f64d1a7fd25be8e31b3ec10
Edison Cury Neto <edisoncuryn@gmail.com>
2018-04-20 00:41:23 -0300

    Starting new contract.

--- truffle.js
+++ truffle.js
@@ -1,11 +1,3 @@
 module.exports = {
-  networks: {
-    live: {
-      network_id: '*',
-      host: "bclohatheous.eastus.cloudapp.azure.com",
-      port: 8545,
-      gas: 3000000,
-      from: "003eb10e4b61f8f0ee4e24031879ac3ea4ed7e02"
-    }
-  }
+  
 };
